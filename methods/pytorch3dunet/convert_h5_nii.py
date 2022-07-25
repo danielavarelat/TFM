c4201f5df882e72d1a07a7c2949e14f2a065aea0
Daniela Varela Tabares <danielavarelatabares@gmail.com>
2022-07-25 08:40:02 +0200

    pytortch3dunet

--- methods/pytorch3dunet/convert_h5_nii.py
+++ methods/pytorch3dunet/convert_h5_nii.py
@@ -3,46 +3,32 @@ import numpy as np
 import h5py
 import nibabel as nib
 import sys
+import json
 
 
 if __name__ == "__main__":
-    # mem = "/homedtic/dvarela/pretrained/pytunet3D/mytest_mem/20190401_E2_mGFP_CardiacRegion_0.5_ZXY_predictions.h5"
-    # pred_zxy = dset = np.array(h5py.File(mem, "r")["predictions"])
-    # print(pred_zxy.shape)
-    # save_path = "/homedtic/dvarela/dataT/20190401_E2_mGFP_CardiacRegion_0.5_ZXY_predictions_pnas.nii.gz"
-    # ni_img = nib.Nifti1Image(pred_zxy, affine=np.eye(4))
-    # nib.save(ni_img, save_path)
-
-    especimens = [
-        "20190119_E1",
-        "20190208_E2",
-        "20190401_E1",
-        "20190404_E1",
-        "20190401_E2",
+    f = open("/homedtic/dvarela/specimens.json")
+    data = json.load(f)
+    flatten_list = [
+        element
+        for sublist in [data[i] for i in ["stage1", "stage2", "stage3", "stage4"]]
+        for element in sublist
     ]
-    files = [
-        os.path.join(
-            "/homedtic/dvarela/RESULTS/membranes/PNAS",
-            e + "_mGFP_CardiacRegion_0.5_ZYX_predictions.h5",
+    mems = "/homedtic/dvarela/CROPS"
+    for sp in flatten_list:
+        file_h5 = os.path.join(
+            mems, f"2019{sp}_mGFP_CardiacRegion_0.5_ZXY_predictions.h5"
         )
-        for e in especimens
-    ]
-    for file_h5 in files:
-        print(file_h5)
+        print(f"RUNNING {file_h5}")
         pred_zxy = np.array(h5py.File(file_h5, "r")["predictions"])
         print(pred_zxy.shape)
-        ni_img = nib.Nifti1Image(pred_zxy, affine=np.eye(4))
-        nib.save(ni_img, file_h5.replace("h5", "nii.gz"))
-    files = [
-        os.path.join(
-            "/homedtic/dvarela/RESULTS/membranes/UNet3D",
-            e + "_mGFP_CardiacRegion_0.5_ZYX_predictions.h5",
+        pred_xyz = np.swapaxes(np.swapaxes(pred_zxy[0, :, :, :], 0, 2), 0, 1)
+        print(pred_xyz.shape)
+        ni_img = nib.Nifti1Image(pred_xyz, affine=np.eye(4))
+        res = "/homedtic/dvarela/RESULTS/membranes/GASP_PNAS"
+        nib.save(
+            ni_img,
+            os.path.join(
+                res, f"2019{sp}_mGFP_CardiacRegion_0.5_XYZ_predictions.nii.gz"
+            ),
         )
-        for e in especimens
-    ]
-    for file_h5 in files:
-        print(file_h5)
-        pred_zxy = np.array(h5py.File(file_h5, "r")["predictions"])
-        print(pred_zxy.shape)
-        ni_img = nib.Nifti1Image(pred_zxy, affine=np.eye(4))
-        nib.save(ni_img, file_h5.replace("h5", "nii.gz"))
