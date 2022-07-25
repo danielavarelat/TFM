93d634f0da03ef5bf48c1b20235c86fb2e42ae56
Daniela Varela Tabares <danielavarelatabares@gmail.com>
2022-07-25 08:40:32 +0200

    cellpose

--- methods/mycellpose/run_cellpose.py
+++ methods/mycellpose/run_cellpose.py
@@ -6,8 +6,9 @@ from importlib import reload
 
 reload(models)
 import nibabel as nib
-from cellpose.io import logger_setup
 import skimage.io
+from skimage import exposure
+
 
 # sys.path.insert(1, "/Users/dvarelat/Documents/MASTER/TFM/methods")
 sys.path.insert(1, "/homedtic/dvarela")
@@ -18,14 +19,12 @@ USE_GPU = core.use_gpu()
 print(">>> GPU activated? %d" % USE_GPU)
 
 
-logger_setup()
-
-
 def run_3d_one(
     model, array_zyx, outfile, channels=[0, 0], diameter=25
 ):  # pylint: disable=dangerous-default-value
     """."""
     print("Predicting maks, flows, style...")
+
     masks, _, _, _ = model.eval(
         array_zyx,
         channels=channels,
@@ -61,30 +60,42 @@ def run_3d_one(
 
 if __name__ == "__main__":
     model_nuclei = models.Cellpose(gpu=USE_GPU, model_type="nuclei")
-    OUT_FOLDER = "/homedtic/dvarela/pretrained/cellpose"
-
-    region_path = "/homedtic/dvarela/CardiacRegion/20190401_E2/20190401_E2_DAPI_CardiacRegion_0.5_NORM_XYZ.nii.gz"
-
-    file_OUT = os.path.join(OUT_FOLDER, os.path.basename(region_path)).replace(
-        "XYZ.nii.gz", "_MASK_ZXY.nii.gz"
-    )
-    array = u.read_nii_XYZ(region_path, get_ZXY=True)
-    print(array.shape)
-    run_3d_one(model_nuclei, array, file_OUT)
-    ### TODO UN SPECIMEN
-    # modelCellpose = models.Cellpose(gpu=USE_GPU, model_type="nuclei")
-    # model_nuclei = models.Cellpose(gpu=USE_GPU, model_type="nuclei")
-
-    # files_IN = [os.path.join(IN_FOLDER, i) for i in os.listdir(IN_FOLDER)]
-    # # revisar que todo sea de la extension
-    # files_IN = [i for i in files_IN if "nii.gz" in i]
-    # basenames = [os.path.basename(i) for i in files_IN]
-    # print(f"Getting inference from {len(files_IN)} files NII.GZ ")
-    # print(OUT_FOLDER)
-    # files_OUT = [
-    #     os.path.join(OUT_FOLDER, i).replace(".nii.gz", "_MASKZYX.nii.gz")
-    #     for i in basenames
+    # files = [
+    #     "/homedtic/dvarela/CardiacRegion/20190401_E2/20190401_E2_DAPI_CardiacRegion_0.5_equalized_XYZ.nii.gz",
+    #     "/homedtic/dvarela/CardiacRegion/20190401_E2/20190401_E2_DAPI_CardiacRegion_0.5_stretched_XYZ.nii.gz",
     # ]
-    # print(files_OUT[0:10])
-    # for i, crop in enumerate(files_IN):
-    #     run_3d_one(model_nuclei, u.read_nii(crop, cellpose=True), files_OUT[i])
+    # filesout = [
+    #     "/homedtic/dvarela/pretrained/cellpose/20190401_E2_DAPI_CardiacRegion_0.5_equalized_MASK_ZXY.nii.gz",
+    #     "/homedtic/dvarela/pretrained/cellpose/20190401_E2_DAPI_CardiacRegion_0.5_stretched_MASK_ZXY.nii.gz",
+    # ]
+
+    especimens = [
+        # "20190504_E1",
+        # "20190404_E2",
+        # "20190520_E4",
+        # "20190516_E3",
+        # "20190806_E3",
+        # "20190520_E2",
+        # "20190401_E3",
+        # "20190517_E1",
+        # "20190520_E1",
+        "20190401_E1",
+    ]
+    files = [
+        os.path.join(
+            os.path.join("/homedtic/dvarela/CardiacRegion", e),
+            f"{e}_DAPI_CardiacRegion_0.5.nii.gz",
+        )
+        for e in especimens
+    ]
+    filesout = [
+        os.path.join("/homedtic/dvarela/RESULTS/nuclei", f"{e}_MASK_EQ_ZXY.nii.gz")
+        for e in especimens
+    ]
+
+    for i, file_nii in enumerate(files):
+        arrayzxy = u.read_nii_XYZ(file_nii, get_ZXY=True)
+        print(arrayzxy.shape)
+        equalized = exposure.equalize_hist(arrayzxy)
+        print("Equalized")
+        run_3d_one(model_nuclei, equalized, filesout[i])
