4f14e0e56c2653a3c7845fc932cc03f26ce0a1fd
Daniela Varela Tabares <danielavarelatabares@gmail.com>
2022-08-01 14:42:15 +0200

    right left

--- methods/extraction/RL.py
+++ methods/extraction/RL.py
@@ -66,87 +66,74 @@ def angle(v1, v2):
     return math.acos(dotproduct(v1, v2) / (length(v1) * length(v2)))
 
 
+# f = open("/homedtic/dvarela/specimens.json")
+f = open("/Users/dvarelat/Documents/MASTER/TFM/methods/specimens.json")
+data = json.load(f)
+
 FOLDERS = [
-    "1_20190504_E1",
-    "7_20190404_E2",
-    "10_20190520_E4",
-    "3_20190516_E3",
-    "4_20190806_E3",
-    "2_20190520_E2",
-    "5_20190401_E3",
-    "8_20190517_E1",
-    "9_20190520_E1",
-    "6_20190401_E1",
+    element
+    for sublist in [
+        [f"{i[-1]}_2019" + e for e in data[i]]
+        for i in ["stage1", "stage2", "stage3", "stage4"]
+    ]
+    for element in sublist
 ]
 
+if __name__ == "__main__":
+    for folder in FOLDERS:
+        print(folder)
+        ESPECIMEN = folder.split("_")[1] + "_" + folder.split("_")[2]
 
-for folder in FOLDERS:
-    print(folder)
-    ESPECIMEN = folder.split("_")[1] + "_" + folder.split("_")[2]
-    file_mesh = f"/Users/dvarelat/Documents/MASTER/TFM/midS/{ESPECIMEN}_myo_midS.ply"
-    midplane = (
-        f"/Users/dvarelat/Documents/MASTER/TFM/midplanes/{ESPECIMEN}_midplane.ply"
-    )
-    folder_surface = (
-        f"/Users/dvarelat/Documents/MASTER/TFM/DATA/EXTRACTION/SURFACE/{ESPECIMEN}/myo"
-    )
-    image_path = f"/Users/dvarelat/Documents/MASTER/TFM/DATA/RESULTS/membranes/GASP_PNAS/{ESPECIMEN}_mGFP_XYZ_predictions_GASP.nii.gz"
-    DFFILE = f"/Users/dvarelat/Documents/MASTER/TFM/DATA/EXTRACTION/{folder}/{ESPECIMEN}_cell_properties_radiomics.csv"
-    land_json = (
-        f"/Users/dvarelat/Documents/MASTER/TFM/landmarks/{ESPECIMEN}_key_points.json"
-    )
-
-    mesh_mid = trimesh.load_mesh(midplane, process=False)
-    print(mesh_mid.vertices.shape)
-    dim_info = c.load3D_metadata(image_path)
-    vertices_location = np.floor(
-        mesh_mid.vertices / [dim_info["x_res"], dim_info["y_res"], dim_info["z_res"]]
-    ).astype("uint16")
-    vertices_location = vertices_location[vertices_location[:, 2] < dim_info["z_size"]]
-    vertices_location = vertices_location[vertices_location[:, 0] < dim_info["x_size"]]
-    vertices_location = vertices_location[vertices_location[:, 1] < dim_info["y_size"]]
-    print(vertices_location.shape)
-
-    n_points = 1
-    points, faces = mesh_mid.sample(n_points, return_index=True)
-    normal = mesh_mid.face_normals[faces]
-
-    df = pd.read_csv(DFFILE)
-    df["angle"] = df["centroids"].apply(
-        lambda x: angle(
-            normal[0, :], findVec(list(vertices_location[0]), ast.literal_eval(x))
+        ######### INPUTS ----------------------------------------------------------------------------
+        midplane = (
+            f"/Users/dvarelat/Documents/MASTER/TFM/midplanes/{ESPECIMEN}_midplane.ply"
         )
-    )
-    df["degrees"] = df["angle"].apply(lambda x: x * (180.0 / math.pi))
+        folder_surface = f"/Users/dvarelat/Documents/MASTER/TFM/DATA/EXTRACTION/SURFACE/{ESPECIMEN}/myo"
+        gasp_mem = f"/Users/dvarelat/Documents/MASTER/TFM/DATA/RESULTS/membranes/GASP_PNAS/{ESPECIMEN}_mGFP_XYZ_predictions_GASP.nii.gz"
+        DFFILE = f"/Users/dvarelat/Documents/MASTER/TFM/DATA/EXTRACTION/{folder}/{ESPECIMEN}_cell_properties_radiomics.csv"
+        land_json = f"/Users/dvarelat/Documents/MASTER/TFM/landmarks/{ESPECIMEN}_key_points.json"
+        #### --------------------------------------
 
-    with open(land_json) as json_file:
-        data = json.load(json_file)
-    p1 = np.round(data["p1"])
-    vector_l1_plane = findVec(list(vertices_location[0]), p1)
+        mesh_mid = trimesh.load_mesh(midplane, process=False)
+        print(mesh_mid.vertices.shape)
+        dim_info = c.load3D_metadata(gasp_mem)
+        vertices_location = np.floor(
+            mesh_mid.vertices
+            / [dim_info["x_res"], dim_info["y_res"], dim_info["z_res"]]
+        ).astype("uint16")
+        vertices_location = np.array(
+            [
+                v
+                for v in vertices_location
+                if (
+                    v[0] < dim_info["x_size"]
+                    and v[1] < dim_info["y_size"]
+                    and v[2] < dim_info["z_size"]
+                )
+            ]
+        )
+        n_points = 1
+        points, faces = mesh_mid.sample(n_points, return_index=True)
+        normal = mesh_mid.face_normals[faces]
+        df = pd.read_csv(DFFILE)
+        df["angle"] = df["centroids"].apply(
+            lambda x: angle(
+                normal[0, :], findVec(list(vertices_location[0]), ast.literal_eval(x))
+            )
+        )
+        df["degrees"] = df["angle"].apply(lambda x: x * (180.0 / math.pi))
 
-    if angle(normal[0, :], vector_l1_plane) * (180.0 / math.pi) > 90:
-        print("Normal pa la RIGHT")
-        df["RL"] = df["degrees"].apply(lambda x: 1 if x > 90 else 0)
-    else:
-        print("Normal pa la LEFT")
-        df["RL"] = df["degrees"].apply(lambda x: 0 if x > 90 else 1)
-    print(df.groupby(["RL"]).count()["cell_in_props"])
-    df.to_csv(DFFILE, index=False, header=True)
-    
-    ##COLOR MESH
-    
-    # OUT_myo = f"/Users/dvarelat/Documents/MASTER/TFM/DATA/EXTRACTION/cells_RL/{ESPECIMEN}_RL.ply"
-    # FILE = f"/Users/dvarelat/Documents/MASTER/TFM/DATA/EXTRACTION/{folder}/myo/{ESPECIMEN}_MYOmesh.pkl"
+        with open(land_json) as json_file:
+            data = json.load(json_file)
+        p1 = np.round(data["p1"])
+        vector_l1_plane = findVec(list(vertices_location[0]), p1)
 
-    # DF_MYO = df[df.lines == 1]
-    # with open(FILE, "rb") as f:
-    #     readMESHES = pickle.load(f)
-    # featRGB = p.feature_colorRGB(DF_MYO, "RL")
-    # dict_label_rgb = dict(zip(DF_MYO.original_labels, featRGB))
-    # for j, cell_id in enumerate(list(DF_MYO.original_labels)):
-    #     readMESHES[j].visual.vertex_colors = dict_label_rgb[cell_id]
-    # all_readMESHES = p.merge(readMESHES)
-    # print(all_readMESHES)
-    # all_readMESHES.export(OUT_myo)
-    
-    print("----------------------")
+        if angle(normal[0, :], vector_l1_plane) * (180.0 / math.pi) > 90:
+            print("Normal pa la RIGHT")
+            df["RL"] = df["degrees"].apply(lambda x: 1 if x > 90 else 0)
+        else:
+            print("Normal pa la LEFT")
+            df["RL"] = df["degrees"].apply(lambda x: 0 if x > 90 else 1)
+        print(df.groupby(["RL"]).count()["cell_in_props"])
+        df.to_csv(DFFILE, index=False, header=True)
+        print("----------------------------------------------------")
