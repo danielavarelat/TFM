435be4bd3b1343e84c876927036e4bd0a423b2aa
Daniela Varela Tabares <danielavarelatabares@gmail.com>
2022-08-01 14:03:47 +0200

    angles and elong

--- methods/extraction/angles.py
+++ methods/extraction/angles.py
@@ -28,45 +28,47 @@ def get_mainaxis(mesh):
     eigen_i = np.where(np.asarray(eigenvalues == eigenvalues.max()))[0][0]
     return eigenvectors[:, eigen_i]
 
+
 def calculate_elongation(mesh):
     cov = np.cov(mesh.vertices.T)
     eigenvalues, eigenvectors = np.linalg.eig(cov)
-    idx = eigenvalues.argsort()[::-1]   
+    idx = eigenvalues.argsort()[::-1]
     eigenValues = eigenvalues[idx]
     return np.sqrt(eigenValues[1] / eigenValues[0])
 
 
+f = open("/homedtic/dvarela/specimens.json")
+data = json.load(f)
+
+FOLDERS = [
+    element
+    for sublist in [
+        [f"{i[-1]}_2019" + e for e in data[i]]
+        for i in ["stage1", "stage2", "stage3", "stage4"]
+    ]
+    for element in sublist
+]
+
 if __name__ == "__main__":
 
-    # LOCAL
-    # pickle_spl = f"/Users/dvarelat/Documents/MASTER/TFM/DATA/EXTRACTION/features/list_meshes/{ESPECIMEN}_SPL_lines_corr.pkl"
-
-    # DFFILE = f"/Users/dvarelat/Documents/MASTER/TFM/DATA/EXTRACTION/features/{ESPECIMEN}_cell_properties_radiomics.csv"
-    # line_mesh = (
-    #     f"/Users/dvarelat/Documents/MASTER/TFM/lines_ply_spl/line_{ESPECIMEN}_spl_10000.ply"
-    # )
-    # gasp_mem = f"/Users/dvarelat/Documents/MASTER/TFM/DATA/RESULTS/membranes/GASP_PNAS/{ESPECIMEN}_mGFP_XYZ_predictions_GASP.nii.gz"
-    # OUTFILE = f"/Users/dvarelat/Documents/MASTER/TFM/DATA/EXTRACTION/features/orientation/{ESPECIMEN}_angles_spl.csv"
-    # CLUSTER
-    f = open("/homedtic/dvarela/specimens.json")
-    data = json.load(f)
-
-    FOLDERS = [
-        element
-        for sublist in [
-            [f"{i[-1]}_2019" + e for e in data[i]] for i in ["stage1", "stage2", "stage3","stage4"]
-        ]  # "stage1", "stage2", "stage3"
-        for element in sublist
-    ]
     for folder in FOLDERS:
         ESPECIMEN = folder.split("_")[1] + "_" + folder.split("_")[2]
         print(folder)
+        ######### INPUTS ----------------------------------------------------------------------------
+        # pickle_spl = f"/Users/dvarelat/Documents/MASTER/TFM/DATA/EXTRACTION/features/list_meshes/{ESPECIMEN}_SPL_lines_corr.pkl"
+        # DFFILE = f"/Users/dvarelat/Documents/MASTER/TFM/DATA/EXTRACTION/features/{ESPECIMEN}_cell_properties_radiomics.csv"
+        # line_mesh = f"/Users/dvarelat/Documents/MASTER/TFM/lines_ply_spl/line_{ESPECIMEN}_spl_10000.ply"
+        # gasp_mem = f"/Users/dvarelat/Documents/MASTER/TFM/DATA/RESULTS/membranes/GASP_PNAS/{ESPECIMEN}_mGFP_XYZ_predictions_GASP.nii.gz"
+        # OUTFILE = f"/Users/dvarelat/Documents/MASTER/TFM/DATA/EXTRACTION/features/orientation/{ESPECIMEN}_angles_spl.csv"
+
+        # cluster
         pickle_spl = f"/homedtic/dvarela/EXTRACTION/features/list_meshes/{ESPECIMEN}_SPL_lines_corr.pkl"
         gasp_mem = f"/homedtic/dvarela/RESULTS/membranes/GASP_PNAS/{ESPECIMEN}_mGFP_XYZ_predictions_GASP.nii.gz"
         DFFILE = f"/homedtic/dvarela/EXTRACTION/features/{ESPECIMEN}_cell_properties_radiomics.csv"
         line_mesh = f"/homedtic/dvarela/lines_ply_spl/line_{ESPECIMEN}_spl_10000.ply"
         OUTFILE = f"/homedtic/dvarela/EXTRACTION/features/orientation/{ESPECIMEN}_angles_spl.csv"
 
+        #### --------------------------------------
         mesh = trimesh.load_mesh(line_mesh, process=False)
         final_number_faces = 18000
         mesh = mesh.simplify_quadratic_decimation(int(final_number_faces))
@@ -75,6 +77,7 @@ if __name__ == "__main__":
         df = pd.read_csv(DFFILE)
         with open(pickle_spl, "rb") as f:
             readMESHES = pickle.load(f)
+
         # SPLANCHNIC
         df = df[df.spl == 1]
         # MYO
@@ -88,7 +91,7 @@ if __name__ == "__main__":
             props_set = [props[i] for i in indices_props]
             index_vertice = []
             mains = []
-
+            elongation = []
             vertices_location = np.floor(
                 mesh.vertices
                 / [dim_info["x_res"], dim_info["y_res"], dim_info["z_res"]]
@@ -104,7 +107,6 @@ if __name__ == "__main__":
                     )
                 ]
             )
-            
             for i, p in enumerate(props_set):
                 print("Calculating closest vertex...")
                 distances_to_my_centr = [
@@ -114,15 +116,18 @@ if __name__ == "__main__":
                 index = np.argsort(distances_to_my_centr)[0]
                 index_vertice.append(index)
                 mains.append(get_mainaxis(readMESHES[i]))
+                elongation.append(calculate_elongation(readMESHES[i]))
             DF_ANGLES = pd.DataFrame()
             DF_ANGLES["cell_in_props"] = indices_props
             DF_ANGLES["closest_vert"] = index_vertice
+
             angles = []
             for i, normal in enumerate(v_normals[index_vertice]):
                 angles.append(abs(dot(normal, mains[i])))
             DF_ANGLES["angles"] = angles
+            DF_ANGLES["Elongation2"] = elongation
             DF_ANGLES.to_csv(OUTFILE, index=False, header=True)
             print(OUTFILE)
         else:
             print("DIFF TAMAÑO")
-        print("-------------")
+        print("--------------------------")
